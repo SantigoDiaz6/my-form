d45d9bef98b480e535d64c32cadde579b6a615ec
Santiago_Diaz <santiago.diaz11d@gmail.com>
2020-05-06 09:20:47 +1000

    Updating the changes made to the form

--- src/App.js
+++ src/App.js
@@ -2,25 +2,73 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    titulo: '',
+    descripcion: '',
+    completada: false,
+    tareas: []
+  };
+
+  handleChange = (e) => {
+    const titIsCheckbox = ['completada'];
+    const { name } = e.target;
+    const value = titIsCheckbox.includes(name) ? e.target.checked : e.target.value;
+
+    this.setState({ [name]: value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+
+    const { titulo, descripcion, completada } = this.state;
+    const user = { titulo, descripcion, completada };
+    const users = this.state.users.concat(user)
+
+    this.state({
+      users,
+      titulo: '',
+      descripcion: '',
+      completada: false
+    }, () => console.log(this.state))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="titulo">Title</label>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.titulo}
+            name="titulo"
+            id="titulo"
+          />
+          <label htmlFor="descripcion">Description</label>
+          <textarea
+            //type="text"
+            onChange={this.handleChange}
+            value={this.state.descripcion}
+            name="descripcion"
+            id="descripcion"
+          />
+          <label htmlFor="completada">Completed</label>
+          <input
+            type="checkbox"
+            onChange={this.handleChange}
+            value={this.state.completada}
+            name="completada"
+            id="completada"
+          />
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
+
 
 export default App;
